ef4fe6199ab6dc395df2084d8989a42174231727
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-17 11:20:08 +0800

    Initial commit.

---
+++ multi lines.py
@@ -0,0 +1,6 @@
+def details ():
+    Description = "Input Multiple lines of text into text file"
+    Date = "05-16-23"
+    print ("\nDescription: {}\nDate: {}".format (Description , Date))
+    
+details ()
